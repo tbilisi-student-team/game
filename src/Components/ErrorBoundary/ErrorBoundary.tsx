986a55cc92518cac165c2bac087723450544f1ab
Melnikov Aleksei <meln.a.a@outlook.com>
2022-08-06 15:07:21 +0400

    feat: add ErrorBoundary

---
+++ src/Components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import React, { Component, ErrorInfo, ReactNode } from 'react';
+
+interface Props {
+  children?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false
+  };
+
+  public static getDerivedStateFromError(error: Error): State {
+    console.error(error);
+
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('Error:', error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return <h1>Error</h1>;
+    }
+
+    return this.props.children;
+  }
+}

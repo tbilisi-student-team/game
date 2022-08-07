95dfc3d04a102cf7012a31b4a4cd7575ef4f17d5
Melnikov Aleksei <meln.a.a@outlook.com>
2022-08-07 08:59:52 +0400

    fix: types in ErrorBoundary

--- src/Components/ErrorBoundary/ErrorBoundary.tsx
+++ src/Components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,10 +1,10 @@
 import React, { Component, ErrorInfo, ReactNode } from 'react';
 
-interface Props {
+type Props = {
   children?: ReactNode;
 }
 
-interface State {
+type State = {
   hasError: boolean;
 }
 

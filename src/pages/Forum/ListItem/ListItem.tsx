ee6876c0e8cd9027e3ea520fc83e91009d86496f
zenzuk <zenzuk91@ya.ru>
2022-08-16 13:35:43 +0300

    сверстал страницу форума

---
+++ src/pages/Forum/ListItem/ListItem.tsx
@@ -0,0 +1,29 @@
+
+import React, { PropsWithChildren } from 'react';
+import { Link } from 'react-router-dom';
+import { ThreadListItem } from '../types';
+
+
+
+
+type ListItemProps = PropsWithChildren<{
+  data: ThreadListItem;
+}>;
+
+export const ListItem = (props: ListItemProps) => {
+  const { id, title, text, datatime, userName, comments } = props.data;
+
+  return (
+    <tr className={''} key={id}>
+      <td className={'info'}>
+        <h3>
+          <Link to={`${id}`}>{title}</Link>
+        </h3>
+        <p>{text}</p>
+      </td>
+      <td className={'datetime'}>{datatime}</td>
+      <td className={'userName'}>{userName}</td>
+      <td className={'messages'}>{comments}</td>
+    </tr>
+  );
+};

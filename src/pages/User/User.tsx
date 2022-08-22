b29486c1a417ca7849b4ea5360552e70ec3b88e3
AlexeiA <alexei.a3@gmail.com>
2022-08-22 11:47:46 +0300

    Revert "feat: add hocs/RequireAuth"

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export function User () {
+  const { id } = useParams();
+
   return (
     <>
-      <h1>User</h1>
+      <h1>{`User:${id}`}</h1>
     </>
   )
 }

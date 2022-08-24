63c19785e265314d60fbf018db681c60a4988f0d
AlexeiA <alexei.a3@gmail.com>
2022-08-24 19:42:42 +0300

    feat: changeUserPassword is now working

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, FormEvent, useRef, useState } from 'react';
 
 import { BASE_URL } from 'core/httpClient';
+import { ChangeUserPasswordRequest } from 'remoteApi';
 
 import { useAppContext } from 'AppContext';
 import Header from '../../UI/Header';
@@ -10,7 +11,6 @@ import './index.css';
 
 import buddy1 from 'assets/buddy-1.png';
 import buddy2 from 'assets/buddy-2-otr.png';
-// import { ChangeUserPasswordRequest } from 'remoteApi';
 
 export function User () {
   const {
@@ -24,14 +24,23 @@ export function User () {
   }
 
   const [ profileFormData, setProfileFormData ] = useState(user);
-  // const [ passwordData, setPasswordData ] = useState<ChangeUserPasswordRequest & { newPasswordRepeat: string }>({
-  //   oldPassword: '',
-  //   newPassword: '',
-  //   newPasswordRepeat: ''
-  // });
+  const [ passwordData, setPasswordData ] = useState<ChangeUserPasswordRequest & { newPasswordRepeat: string }>({
+    oldPassword: '',
+    newPassword: '',
+    newPasswordRepeat: ''
+  });
 
   const handleChangeInput = (value: string, name: string) => {
     setProfileFormData((prevState) => {
+      return {
+        ...prevState,
+        [name]: value
+      }
+    });
+  }
+
+  const handleChangePasswordInput = (value: string, name: string) => {
+    setPasswordData((prevState) => {
       return {
         ...prevState,
         [name]: value
@@ -58,7 +67,15 @@ export function User () {
   }
 
   const handleChangePassword = () => {
-    alert('Здесь будет смена пароля!');
+    if (passwordData.oldPassword && passwordData.newPassword && passwordData.newPasswordRepeat) {
+      if (passwordData.newPassword === passwordData.newPasswordRepeat) {
+        actions.changeUserPassword({
+          oldPassword: passwordData.oldPassword,
+          newPassword: passwordData.newPassword
+        });
+      }
+      actions.loadingError(new Error('New passwords are not equal'));
+    }
   }
 
   return (
@@ -81,90 +98,119 @@ export function User () {
         <div className='left-character-wrapper left-character-wrapper__main'>
           <img src={buddy1} alt='Buddy One' className='left-character'/>
         </div>
+        <div className='wrapper'>
+          <form onSubmit={handleSubmit}>
+            <Input
+              type='email'
+              id='email'
+              name='email'
+              placeholder='email'
+              required={true}
+              value={profileFormData.email}
+              setValue={handleChangeInput}
+              disabled={state.isLoading}
+            />
 
-        <form className='signin-form' onSubmit={handleSubmit}>
-          <Input
-            type='email'
-            id='email'
-            name='email'
-            placeholder='email'
-            required={true}
-            value={profileFormData.email}
-            setValue={handleChangeInput}
-            disabled={state.isLoading}
-          />
+            <Input
+              type='tel'
+              id='phone'
+              name='phone'
+              placeholder='phone'
+              required={true}
+              value={profileFormData.phone}
+              setValue={handleChangeInput}/>
 
-          <Input
-            type='tel'
-            id='phone'
-            name='phone'
-            placeholder='phone'
-            required={true}
-            value={profileFormData.phone}
-            setValue={handleChangeInput}/>
+            <Input
+              type='text'
+              id='login'
+              name='login'
+              placeholder='login'
+              required={true}
+              value={profileFormData.login}
+              setValue={handleChangeInput}/>
 
-          <Input
-            type='text'
-            id='login'
-            name='login'
-            placeholder='login'
-            required={true}
-            value={profileFormData.login}
-            setValue={handleChangeInput}/>
+            <Input
+              type='text'
+              id='first_name'
+              name='first_name'
+              placeholder='first_name'
+              required={true}
+              value={profileFormData.first_name}
+              setValue={handleChangeInput}/>
 
-          <Input
-            type='text'
-            id='first_name'
-            name='first_name'
-            placeholder='first_name'
-            required={true}
-            value={profileFormData.first_name}
-            setValue={handleChangeInput}/>
+            <Input
+              type='text'
+              id='second_name'
+              name='second_name'
+              placeholder='second_name'
+              required={true}
+              value={profileFormData.second_name}
+              setValue={handleChangeInput}/>
 
-          <Input
-            type='text'
-            id='second_name'
-            name='second_name'
-            placeholder='second_name'
-            required={true}
-            value={profileFormData.second_name}
-            setValue={handleChangeInput}/>
+            <Input
+              type='text'
+              id='display_name'
+              name='display_name'
+              placeholder='display_name'
+              required={true}
+              value={profileFormData.display_name}
+              setValue={handleChangeInput}/>
 
-          <Input
-            type='text'
-            id='display_name'
-            name='display_name'
-            placeholder='display_name'
-            required={true}
-            value={profileFormData.display_name}
-            setValue={handleChangeInput}/>
+            <button
+              disabled={state.isLoading}
+              type={'submit'}
+              className={'button'}
+            >
+              <span className={'button-title'}>
+                Save
+              </span>
+            </button>
+          </form>
 
-          <button
-            disabled={state.isLoading}
-            type={'button'}
-            className={'button'}
-            onClick={handleChangePassword}
-          >
-            <span className={'button-title'}>
-              Change Password
-            </span>
-          </button>
+          <form onSubmit={handleSubmit}>
+            <Input
+              type='password'
+              id='oldPassword'
+              name='oldPassword'
+              placeholder='Current password'
+              required={true}
+              value={passwordData.oldPassword}
+              setValue={handleChangePasswordInput}/>
 
-          <button
-            disabled={state.isLoading}
-            type={'submit'}
-            className={'button'}
-          >
-            <span className={'button-title'}>
-              Save
-            </span>
-          </button>
+            <Input
+              type='password'
+              id='newPassword'
+              name='newPassword'
+              placeholder='New password'
+              required={true}
+              value={passwordData.newPassword}
+              setValue={handleChangePasswordInput}/>
+
+            <Input
+              type='password'
+              id='newPasswordRepeat'
+              name='newPasswordRepeat'
+              placeholder='Repeat new password'
+              required={true}
+              value={passwordData.newPasswordRepeat}
+              setValue={handleChangePasswordInput}/>
+
+            <button
+              disabled={state.isLoading}
+              type={'submit'}
+              className={'button'}
+              onClick={handleChangePassword}
+            >
+              <span className={'button-title'}>
+                Change Password
+              </span>
+            </button>
+          </form>
 
           {state.error && (
             <div>{state.error.message}</div>
           )}
-        </form>
-
+        </div>
         <div className='right-character-wrapper right-character-wrapper__main'>
           <img src={buddy2} alt='Buddy Two' className='right-character'/>
         </div>

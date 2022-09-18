30ec5752dbea68d80d5dd7fa060d78604500fa31
AlexeiA <alexei.a3@gmail.com>
2022-09-18 16:56:03 +0300

    fix !HACK! yp baseUrl

--- src/config/AxiosRequestToYPTechConfig.ts
+++ src/config/AxiosRequestToYPTechConfig.ts
@@ -3,6 +3,6 @@ import {AxiosRequestConfig} from 'axios';
 import {YP_TECH_BASE_URL} from "@/config/index";
 
 export const AXIOS_REQUEST_TO_YP_TECH_CONFIG: AxiosRequestConfig = {
-  baseURL: YP_TECH_BASE_URL,
+  baseURL: 'https://ya-praktikum.tech/api/v2',//TODO hack to fix undefined
   timeout: 20000,
 }

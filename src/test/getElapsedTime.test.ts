a1d6c9ca255dfa2b6e9895642d83bc61dfd50842
AlexeiA <alexei.a3@gmail.com>
2022-10-27 14:29:59 +0300

    move *.test.ts for next build

--- src/test/getElapsedTime.test.ts
+++ src/test/getElapsedTime.test.ts
@@ -1,4 +1,4 @@
-import { getElapsedTime } from './getElapsedTime';
+import { getElapsedTime } from '../pages/game/utils/getElapsedTime';
 
 test('getElapsedTime', () => {
   expect(getElapsedTime(100, 0, 50)).toBe(50);
--- src/test/getPauseTime.test.ts
+++ src/test/getPauseTime.test.ts
@@ -1,4 +1,4 @@
-import { getPauseTime } from './getPauseTime';
+import { getPauseTime } from '../pages/game/utils/getPauseTime';
 
 test('getPauseTime', () => {
   expect(getPauseTime(100, 0, 50)).toBe(50);
